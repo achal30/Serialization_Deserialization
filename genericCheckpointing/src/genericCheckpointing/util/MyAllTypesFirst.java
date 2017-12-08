f3b7869749a7c35889998035ba9de0b227c6ce44
Achal <apatel58@binghamton.edu>
2017-12-08 10:04:47 -0500

    added getter and setters for the data members

--- genericCheckpointing/src/genericCheckpointing/util/MyAllTypesFirst.java
+++ genericCheckpointing/src/genericCheckpointing/util/MyAllTypesFirst.java
@@ -1,5 +1,91 @@
 package genericCheckpointing.util;
 
-public class MyAllTypesFirst {
+public class MyAllTypesFirst extends SerializableObject{
+	
+	private int myInt;
+	private int myOtherInt;
+	private long myLong;
+	private long myOtherLong;
+	private boolean myBool;
+	private String myString;
+	
+	
+	public MyAllTypesFirst(int myIntIn, int myOtherIntIn, long myLongIn, long myOtherLongIn, boolean myBoolIn, String myStringIn){
+		
+		myInt = myIntIn;
+		myOtherInt = myOtherIntIn;
+		myLong = myLongIn;
+		myOtherLong = myOtherLongIn;
+		myBool = myBoolIn;
+		myString = myStringIn;
+	}
+	
 
+	public int getMyInt(){
+		return myInt;
+	}
+	
+	public void setMyInt(int myIntIn){
+		myInt = myIntIn;
+	}
+	
+	public int getMyOtherInt(){
+		return myOtherInt;
+	}
+	
+	public void setMyOtherInt(int myOtherIntIn){
+		myOtherInt = myOtherIntIn;
+	}
+	
+	public long getMyLong(){
+		return myLong;
+	}
+	
+	public void setMyLong(long myLongIn){
+		myLong = myLongIn;
+	}
+	
+	public long getMyOtherLong(){
+		return myOtherLong;
+	}
+	
+	public void setMyOtherLong(long myOtherLongIn){
+		myOtherLong = myOtherLongIn;
+	}
+	
+	public boolean getMyBool(){
+		return myBool;
+	}
+	
+	public void setMyBool(boolean myBoolIn){
+		myBool = myBoolIn;
+	}
+	
+	public String getMyString(){
+		return myString;
+	}
+	
+	public void setMyString(String myStringIn){
+		myString = myStringIn;
+	}
+	
+	@Override
+	public boolean equals(Object O){
+		if(!(O instanceof MyAllTypesFirst)){
+			return false;
+		}
+		MyAllTypesFirst obj = (MyAllTypesFirst) O;
+		if(this.myInt == obj.getMyInt() && this.myBool == obj.getMyBool() && this.myLong == obj.getMyLong() && this.myOtherInt == obj.getMyOtherInt() && this.myOtherLong == obj.getMyOtherLong() && this.myString.equals(obj.getMyString())){
+			return true;
+		}
+		return false;
+	}
+	
+	@Override
+	public int hashCode(){
+		return (myInt + myOtherInt);
+	}
+	
+	
 }
+
--- genericCheckpointing/src/genericCheckpointing/util/MyAllTypesSecond.java
+++ genericCheckpointing/src/genericCheckpointing/util/MyAllTypesSecond.java
@@ -1,5 +1,88 @@
 package genericCheckpointing.util;
 
-public class MyAllTypesSecond {
+public class MyAllTypesSecond extends SerializableObject{
+	
+	private double myDoubleT;
+	private double myOtherDoubleT;
+	private float myFloatT;
+	private short myShortT;
+	private short myOtherShortT;
+	private char myCharT;
+	
+	public MyAllTypesSecond(double myDoubleTIn, double myOtherDoubleTIn, float myFloatTIn, short myShortTIn, short myOtherShortTIn, char myCharTIn){
+		
+		myDoubleT = myDoubleTIn;
+		myOtherDoubleT = myOtherDoubleTIn;
+		myFloatT = myFloatTIn;
+		myShortT = myShortTIn;
+		myOtherShortT = myOtherShortTIn;
+		myCharT = myCharTIn;
+		
+	}
+	
+	
+	public double getMyDoubleT(){
+		return myDoubleT;
+	}
+	
+	public void setMyDoubleT(double myDoubleTIn){
+		myDoubleT = myDoubleTIn;
+	}
 
+	public double getMyOtherDoubleT(){
+		return myOtherDoubleT;
+	}
+	
+	public void setMyOtherDoubleT(double myOtherDoubleTIn){
+		myOtherDoubleT = myOtherDoubleTIn;
+	}
+	
+	public float getMyFLoatT(){
+		return myFloatT;
+	}
+	
+	public void setMyFloatT(float myFloatTIn){
+		myFloatT = myFloatTIn;
+	}
+	
+	public short getMyShortT(){
+		return myShortT;
+	}
+	
+	public void setMyShortT(short myShortTIn){
+		myShortT = myShortTIn;
+	}
+	
+	public short getMyOtherShortT(){
+		return myOtherShortT;
+	}
+	
+	public void setMyOtherShortT(short myOtherShortTIn){
+		myOtherShortT = myOtherShortTIn;
+	}
+	
+	public char getMyCharT(){
+		return myCharT;
+	}
+	
+	public void setMyChar(char myCharTIn){
+		myCharT = myCharTIn;
+	}
+	
+	@Override
+	public boolean equals(Object O){
+		if(!(O instanceof MyAllTypesSecond)){
+			return false;
+		}
+		MyAllTypesSecond obj = (MyAllTypesSecond) O;
+		if(this.myCharT == obj.getMyCharT() && this.myShortT == obj.myShortT && this.myOtherShortT == obj.getMyOtherShortT() && this.myFloatT == obj.getMyFLoatT() && this.myDoubleT == obj.myDoubleT && this.myOtherDoubleT == obj.myOtherDoubleT ){
+			return true;
+		}
+		return false;
+	}
+	
+	@Override
+	public int hashCode(){
+		return (myShortT + myOtherShortT);
+	}
 }
